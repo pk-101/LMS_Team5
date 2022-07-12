438348eab0122a390847fd40e9a22b4fbce9a2aa
pk-101 <pk4799335@gmil.com>
2022-07-12 17:45:41 +0530

    I have added employee model

---
+++ LMS_Team5/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Team5.Model
+{
+    public class Employee
+    {
+        public int Emp_Id { get; set; }
+        public string Emp_Name { get; set; }
+        public string Emp_Email { get; set; }
+        public long Emp_Phone { get; set; }
+        public DateTime Emp_Doj { get; set; }
+        public string Emp_Dept { get; set; }
+        public int Emp_LeaveBal { get; set; }
+    }
+}
