68d3001faf6cbb2b705066103201af3f2c147f08
kkk362 <karankhandelwal123510@gmail.com>
2022-07-12 17:53:56 +0530

    I haved added manager model

---
+++ LMS_Team5/Model/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Team5.Model
+{
+    public class Manager
+    {
+        public int Man_Id { get; set; }
+        public string Man_Name { get; set; }
+        public string Man_Email { get; set; }
+        public long Man_Phone { get; set; }
+    }
+}
